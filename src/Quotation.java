17382e9ee9ac91e88e449784ff73f8e110fef080
Aleksey-Shulzhenko <160366926+Aleksey-Shulzhenko@users.noreply.github.com>
2025-02-03 23:28:47 +0200

    Update Quotation.java

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,6 +1,9 @@
 public class Quotation {
-    String quote = "Welcome to Sun!";
+    String quote = "What you do speaks so loudly that I cannot hear what you say.";
+    String author = "Ralph Waldo Emerson"; // ім'я автора цитати
+
     public void display() {
-      System.out.println(quote);
+        System.out.println(quote);
+        System.out.println("– " + author); // додати ім'я автора до виводу
     }
-  }
+}
