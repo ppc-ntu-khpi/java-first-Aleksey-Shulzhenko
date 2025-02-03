ed970c26d2a4e81e6a6f59c7d741741ed5bf1115
Aleksey-Shulzhenko <160366926+Aleksey-Shulzhenko@users.noreply.github.com>
2025-02-03 23:29:38 +0200

    Update Shirt.java

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,17 +1,18 @@
 public class Shirt {
-    public int shirtID = 0; // стандартне значення номера моделі сорочки
-    public String description = "-description required-"; // стандартний опис сорочки
-    // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
-    public char colorCode = 'U';
-    public double price = 0.0; // стандартна вартість сорочки
-    public int quantityInStock = 0; // стандартна кількість на складі
+    public int shirtID = 1234; // довільне значення номера моделі сорочки
+    public String description = "Classic white shirt"; // довільний опис сорочки
+    // коди кольорів: red=червоний, blue=синій, green=зелений, unset=невідомо
+    public String colorCode = "unset"; // початкове значення кольору
+    public double price = 29.99; // довільна вартість сорочки
+    public int quantityInStock = 100; // довільна кількість на складі
 
     // цей метод просто виводить всю інформацію про сорочку на екран
     public void displayShirtInformation() {
         System.out.println("Shirt ID: " + shirtID);
-        System.out.println("Shirt description:" + description);
+        System.out.println("Shirt description: " + description);
         System.out.println("Color Code: " + colorCode);
         System.out.println("Shirt price: $" + price);
         System.out.println("Quantity in stock: " + quantityInStock);
     } // кінець методу displayShirtInformation
 } // кінець опису класу
+
